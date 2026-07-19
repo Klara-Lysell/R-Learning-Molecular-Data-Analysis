f33fa8704b36aa677f92ead382353a5db6b1901e
Klara Lysell <lysellklara@gmail.com>
2026-07-19 23:06:46 +0200

    Update Week 1 Exercises part 2.R

--- HarvardX-Data-Science-R/Week 1/Week 1 Exercises part 2.R
+++ HarvardX-Data-Science-R/Week 1/Week 1 Exercises part 2.R
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-mice <- read.csv("femaleMiceWeights.csv")
-names(mice)
-mice[12,2]
-mice$Bodyweight[11]
-length(mice$Bodyweight)
-mice
-mice$Diet=="hf"
-hf_mice <- mice[mice$Diet == "hf", ]
-mean(hf_mice$Bodyweight)
-set.seed(1)
-sample(13:24, size = 1)
-mice$Bodyweight[21]
-=======
 # Read the female mice dataset from a CSV file
 mice <- read.csv("femaleMiceWeights.csv")
 
